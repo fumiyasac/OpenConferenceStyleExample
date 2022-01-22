5cf2389a220e346f2e45783604200a053210bc43
Fumiya Sakai <just1factory@gmail.com>
2022-01-22 23:59:36 +0900

    Initial Commit

---
+++ OpenConferenceStyleExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OpenConferenceStyleExample
+//
+//  Created by 酒井文也 on 2022/01/22.
+//
+
+// Multi Module構成 (uhooi PicBooks)
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
